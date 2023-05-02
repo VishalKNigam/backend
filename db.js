d1caff6582175e1ba67a74fea0df15dcd3907cbb
VishalKNigam <vishalnigam1407@gmail.com>
2023-05-02 18:21:38 +0530

    s

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose");
-const connection = mongoose.connect("mongodb://127.0.0.1:27017/blog");
+const connection = mongoose.connect("mongodb+srv://vishalnigam1407:vishal1995@cluster0.tynh4fa.mongodb.net/app?retryWrites=true&w=majority");
 module.exports = {connection};
